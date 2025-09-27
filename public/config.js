a51a2c15c8a0fa7b5a4e8179b6dd59cd7ad9338a
cyyy666 <319213657@qq.com>
2025-09-27 13:18:02 +0800

    更新配置

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '叶子监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur3079068-6877f4a77303e87198be5df7',
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '刷新',
+      url: 'https://status.cy00.top'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/chuiyejiangyaobaobao'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '博客',
+      url: 'https://status.cy00.top'
     },
   ],
 };
